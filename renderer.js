1aa090e7b04fe29e9bd276cccb4608dc4de8e05b
0xearl <earljohnsabalo@gmail.com>
2023-02-23 07:30:02 +0800

    removed unnecessary code

--- renderer.js
+++ renderer.js
@@ -1,10 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
     api.hasFile((file) => {
-        const imageDisplay = document.getElementById('imageDisplay')
-        const uploadForm = document.getElementById('uploadForm')
-        imageDisplay.classList.remove('hidden')
-        uploadForm.classList.add('hidden')
-        imageDisplay.src = file;
+        if(file !== '') {
+            const imageDisplay = document.getElementById('imageDisplay')
+            const uploadForm = document.getElementById('uploadForm')
+            imageDisplay.classList.remove('hidden')
+            uploadForm.classList.add('hidden')
+            imageDisplay.src = file;
+        }
     })
 
     const fileUpload = document.getElementById('imageSelect')
@@ -19,14 +21,4 @@ window.addEventListener('DOMContentLoaded', () => {
         uploadForm.classList.add('hidden')
         imageDisplay.src = file;
     })
-})
-
-window.addEventListener('keypress', (event) => {
-    if(event.key == 'c') {
-        api.openDialog()
-    }
-
-    if(event.key == 'n') {
-        api.spawnWindow()
-    }
 })
